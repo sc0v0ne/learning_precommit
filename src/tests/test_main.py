5ba4edb60223d1bc3a0afa564f0736f641a4a94b
sc0v0ne <sc0v1n001@gmail.com>
2024-07-19 22:16:54 -0300

    Update linter

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -3,10 +3,12 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hey, Python!!!!!"}
+
 
 def test_read_item():
     response = client.get("/items/42?q=test")
